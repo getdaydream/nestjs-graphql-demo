065131d6aecfec09d5be147bab06d1a81c939c04
getdaydream <getdaydream@gmail.com>
2018-07-11 23:10:44 +0800

    reset 2

--- app/router.ts
+++
@@ -1,8 +0,0 @@
-import { Application } from 'egg';
-
-export default (app: Application) => {
-  const { controller, router } = app;
-
-  router.get('/user', controller.user.find);
-  router.post('/user', controller.user.create);
-};
--- typings/index.d.ts
+++
@@ -1,5 +0,0 @@
-import app from "../app"; // 如果不引入，类型检查会错误
-
-declare module 'egg' {
-    
-}
