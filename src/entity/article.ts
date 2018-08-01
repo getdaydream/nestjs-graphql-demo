d30187818cf324aba15f17d44fdec6c55158e35b
Dong Haoran <donghaoran@palmax.cn>
2018-08-01 19:09:16 +0800

    temp

--- src/entity/article.ts
+++ src/entity/article.ts
@@ -1,8 +1,14 @@
-import { Column, Entity } from 'typeorm';
+import { Column, Entity, OneToMany, ManyToMany, JoinTable } from 'typeorm';
 import { Post } from './post';
+import { Tag } from './tag';
 
-@Entity()
 export  class Article extends Post {
+@Entity()
   @Column()
   published: boolean = false;
+
+  // this will generate article_tags table
+  @ManyToMany(type => Tag)
+  @JoinTable()
+  tags: Tag[];
 }
